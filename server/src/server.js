344475fc26da4bed4c268b07f5c9f8f070a2d50c
James Tennant <jtennant@ucsc.edu>
2023-09-06 16:55:29 -0700

    basic socket.io server

---
+++ server/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = 3000;
+
+app.use(express.static('public'));
+
+http.listen(port, () => console.log(`Listening on *:${port}`));
+
+// broadcast movement at 60 Hz
+setInterval(() => {
+  io.volatile.emit('move', Math.random());
+}, 1000 / 60);
+
+// handle client connection
+io.on('connect', (socket) => {
+  console.log('connect!');
+
+  socket.on('disconnect', () => {
+    console.log('disconnect!');
+  });
+});
