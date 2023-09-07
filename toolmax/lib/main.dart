466a04e7ddb7b4ae56a957c05f6f0211c2d7a45c
James Tennant <jtennant@ucsc.edu>
2023-09-06 18:14:35 -0700

    connect to server

--- toolmax/lib/main.dart
+++ toolmax/lib/main.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 
+// ignore: library_prefixes
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  static final IO.Socket _socket = IO.io(
+    'http://localhost:3000',
+    IO.OptionBuilder().setTransports(['websocket']).build(),
+  );
+
+  _connectSocket() {
+    _socket.onConnect((_) => print('Connection established'));
+    _socket.onConnectError((data) => print('Connect Error: $data'));
+    _socket.onDisconnect((_) => print('Socket. I0 server disconnected'));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _connectSocket();
+  }
 
   @override
   Widget build(BuildContext context) {
